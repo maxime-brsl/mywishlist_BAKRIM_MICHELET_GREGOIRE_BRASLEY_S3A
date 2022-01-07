a625fd98e75c5e13439f14810520cf944072713d
Maxime Brasley <75072652+maxime-brsl@users.noreply.github.com>
2022-01-07 11:37:25 +0100

    vrai correction pour bug JS pour bouton ajouter liste

--- wishlist/js/listener.js
+++ wishlist/js/listener.js
@@ -6,7 +6,7 @@ let boutonAddList = document.querySelector("#baddlist");
 let formulaireItem = document.querySelector("#fitem");
 let formulaireListe = document.querySelector("#flist");
 let formulaireAjoutItem = document.querySelector("#fajoutitem");
-let formulaireAddListe = document.querySelector("#faddliste");
+let formulaireAddListe = document.querySelector("#faddlist");
 
 formulaireItem.style.display = "none";
 formulaireListe.style.display = "none";
@@ -17,23 +17,26 @@ boutonListe.addEventListener("click", () => {
     formulaireListe.style.display = "block";
     formulaireItem.style.display = "none";
     formulaireAjoutItem.style.display = "none";
-    formulaireAddListe.style.display = "block";
+    formulaireAddListe.style.display = "none";
 });
 
 boutonItem.addEventListener("click", () => {
     formulaireItem.style.display = "block";
     formulaireListe.style.display = "none";
     formulaireAjoutItem.style.display = "none";
-    formulaireAddListe.style.display = "block";
+    formulaireAddListe.style.display = "none";
 });
 
 boutonAjoutItem.addEventListener("click", () => {
     formulaireItem.style.display = "none";
     formulaireListe.style.display = "none";
     formulaireAjoutItem.style.display = "block";
-    formulaireAddListe.style.display = "block";
+    formulaireAddListe.style.display = "none";
 });
 
 boutonAddList.addEventListener("click", () => {
+    formulaireItem.style.display = "none";
+    formulaireListe.style.display = "none";
+    formulaireAjoutItem.style.display = "none";
     formulaireAddListe.style.display = "block";
 });
