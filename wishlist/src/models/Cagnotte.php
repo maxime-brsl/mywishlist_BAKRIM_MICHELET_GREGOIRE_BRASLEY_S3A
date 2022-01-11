37d0bd7c6f90d5aafba5e841d9f4f7798a82e427
Maxime Brasley <75072652+maxime-brsl@users.noreply.github.com>
2022-01-11 11:10:27 +0100

    Modification commentaire ajout table cagnotte

--- wishlist/src/models/Cagnotte.php
+++ wishlist/src/models/Cagnotte.php
@@ -7,7 +7,14 @@ namespace mywishlist\models;
  *
  * requete sql a taper pour creer la table cagnotte
  *
- * CREATE TABLE cagnotte(id_item int(11), montant int(11),
+ * CREATE TABLE cagnotte(
+ * item_id number(11),
+ * liste_id number(11)
+ * montant number(11),
+ * user_id number(11),
+ * FOREIGN KEY (item_id) REFERENCES item(id),
+ * FOREIGN KEY (liste_id) REFERENCES liste(no)
+ *    )
  *
  **/
 
