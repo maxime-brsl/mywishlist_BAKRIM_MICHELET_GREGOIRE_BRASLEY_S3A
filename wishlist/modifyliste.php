ec53fe2b4ad966ee77af74efa48280a1da77e2f2
Bakrim Yassine <yassine.bakrim7@etu.univ-lorraine.fr>
2022-01-23 20:42:10 +0100

    modifications du modifyliste

--- wishlist/modifyliste.php
+++ wishlist/modifyliste.php
@@ -9,21 +9,20 @@ $ntitre = filter_var($_POST['newtitle'], FILTER_SANITIZE_STRING);
 $ndesc = filter_var($_POST['newdesc'], FILTER_SANITIZE_STRING);
 $nexp = filter_var($_POST['newexp'], FILTER_SANITIZE_STRING);
 
-$liste = \mywishlist\models\Liste::where('token', '=', $token)->first();
+$liste = \mywishlist\models\Liste::where('token', '=', $token);
 
 if($ntitre != ""){
-    $liste->titre = $ntitre;
+    $liste->update(['titre' => $ntitre]);
 }
 
 if($ndesc != ""){
-    $liste->description = $ndesc;
+    $liste->update(['description' => $ndesc]);
 }
 
 if($nexp != ""){
-    $liste->expiration = $nexp;
+    $liste->update(['expiration' => $nexp]);
 }
 
-$liste->save();
 
 header("Location: http://localhost/wishlist/liste/$token");
 exit();
